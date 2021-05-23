5f99021a1317284ef8d303aadd3844bf417b2404
Nicolas Bouhours <n.bouhours53@gmail.com>
2021-05-23 20:49:21 +0200

    Ajoutez des fichiers projet.

---
+++ BookStore.API/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using BookStore.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore.API.Controllers
+{
+    /// <summary>
+    /// This is a test API Controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILoggerService _logger;
+
+        public HomeController(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+
+
+
+        /// <summary>
+        /// Home Message
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Index()
+        {
+            _logger.LogInfo("Accessed to Home Controller");
+            return Ok("Hello World");
+        }
+    }
+}
---
+++ BookStore.API/Services/ILoggerService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Services
+{
+    public interface ILoggerService
+    {
+        void LogDebug(string message);
+
+        void LogInfo(string message);
+
+        void LogWarning(string message);
+
+        void LogError(string message);
+
+     
+    }
+}
